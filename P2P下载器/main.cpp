affc2f7cf755ff97222f4280b025501340751b00
zhaomengchao <1935812591@qq.com>
2020-02-11 20:32:17 +0800

    补充main函数

--- P2P下载器/main.cpp
+++ P2P下载器/main.cpp
@@ -1,9 +1,20 @@
 #include"util.hpp"
+#include"httplib.h"
+
+void helloworld(const httplib::Request &req, httplib::Response &rsp)
+{
+	rsp.set_content("<html><h1>HelloWord</h1></html>", "text/html");
+	rsp.status = 200;
+}
 
 int main(int argc, char *argv[])
 {
-	std::vector<Adapter> list;
-	AdapterUtil::GetAllAdapter(&list);
-	system("pause");
+	/*std::vector<Adapter> list;
+	AdapterUtil::GetAllAdapter(&list);*/
+	httplib::Server srv;
+
+	srv.Get("/", helloworld);
+	srv.listen("0.0.0.0", 9000);
+    system("pause");
 	return 0;
 }
